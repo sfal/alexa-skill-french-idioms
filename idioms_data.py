0533b7b4cb690c1815cc3bab35bb3719ef88f578
sfal <45284370+sfal@users.noreply.github.com>
2019-03-27 08:45:45 +0100

    Uploading French idioms
    Dictionary of idioms with meaning in Italian

---
+++ idioms_data.py
@@ -0,0 +1,598 @@
+data = [
+    {
+        "Idioma": "C’est simple comme bonjour !",
+        "Significato": "Facile come bere un bicchiere d’acqua!"
+    },
+    {
+        "Idioma": "On n’est pas sorti de l’auberge !",
+        "Significato": "Non ne siamo ancora usciti!"
+    },
+    {
+        "Idioma": "Être dans la galère",
+        "Significato": "Essere nei guai"
+    },
+    {
+        "Idioma": "Il ne faut pas pousser mémé dans les orties !",
+        "Significato": "Non bisogna esagerare!"
+    },
+    {
+        "Idioma": "Ça ne casse pas trois pattes à un canard",
+        "Significato": "Non è niente di speciale"
+    },
+    {
+        "Idioma": "Pisser dans un violon",
+        "Significato": "Parlare per niente"
+    },
+    {
+        "Idioma": "Chacun voit midi à sa porte",
+        "Significato": "Giudicare qualcuno o qualcosa solo in base ai propri criteri"
+    },
+    {
+        "Idioma": "Au petit bonheur la chance",
+        "Significato": "Chi vivrà vedrà"
+    },
+    {
+        "Idioma": "À bon chat, bon rat",
+        "Significato": "Rendere pan per focaccia"
+    },
+    {
+        "Idioma": "Faire un froid de canard",
+        "Significato": "Fare un freddo cane"
+    },
+    {
+        "Idioma": "Brider l’âne par la queue",
+        "Significato": "Compiere azioni prive di senso logico"
+    },
+    {
+        "Idioma": "Laisser pisser le mérinos",
+        "Significato": "Meglio non reagire alle provocazioni"
+    },
+    {
+        "Idioma": "Quand on parle du loup",
+        "Significato": "Parli del diavolo…"
+    },
+    {
+        "Idioma": "Sauter du coq à l’âne",
+        "Significato": "Saltare di palo in frasca"
+    },
+    {
+        "Idioma": "Avoir le cafard",
+        "Significato": "Avere nostalgia di qualcosa"
+    },
+    {
+        "Idioma": "Donner de la confiture aux cochons",
+        "Significato": "Dare la pelle ai porci"
+    },
+    {
+        "Idioma": "Raconter des salades",
+        "Significato": "Raccontare menzogne"
+    },
+    {
+        "Idioma": "Se faire rouler dans la farine",
+        "Significato": "Farsi ingannare da qualcuno"
+    },
+    {
+        "Idioma": "En faire tout un fromage",
+        "Significato": "Farla lunga"
+    },
+    {
+        "Idioma": "Courir sur le haricot de quelqu’un",
+        "Significato": "Infastidire qualcuno"
+    },
+    {
+        "Idioma": "Il y a de l’eau dans le gaz",
+        "Significato": "C’è attrito tra due persone"
+    },
+    {
+        "Idioma": "Il faut tourner sept fois sa langue dans sa bouche avant de parler",
+        "Significato": "Contare fino a dieci prima di parlare"
+    },
+    {
+        "Idioma": "Avoir des atomes crochus avec quelqu’un",
+        "Significato": "Avere molte cose in comune con qualcuno"
+    },
+    {
+        "Idioma": "Avoir un petit vélo dans la tête",
+        "Significato": "Avere molta fantasia e creatività"
+    },
+    {
+        "Idioma": "Faire du lèche-vitrines",
+        "Significato": "Girare per negozi"
+    },
+    {
+        "Idioma": "Etre au bout du rouleau",
+        "Significato": "Non farcela più per la stanchezza"
+    },
+    {
+        "Idioma": "Broyer du noir",
+        "Significato": "Vedere tutto nero"
+    },
+    {
+        "Idioma": "Être maquillée comme una voiture volée",
+        "Significato": "Essere troppo truccati"
+    },
+    {
+        "Idioma": "Il ne faut pas jamais dire: fontaine, je ne boirai pas de ton eau",
+        "Significato": "Mai dire mai"
+    },
+    {
+        "Idioma": "La faim chasse le loup hors du bois",
+        "Significato": "Agire per necessità di qualcosa"
+    },
+    {
+        "Idioma": "L’union fait la force",
+        "Significato": "L’unione fa la forza"
+    },
+    {
+        "Idioma": "Sage est le juge qui écoute et tard juge",
+        "Significato": "L’apparenza inganna"
+    },
+    {
+        "Idioma": "Grands vanteurs, petits faiseurs",
+        "Significato": "Chi si vanta vale ben poco"
+    },
+    {
+        "Idioma": "Deux précautions valent mieux qu’une",
+        "Significato": "Meglio prevenire che curare"
+    },
+    {
+        "Idioma": "Goutte à goutte l’eau creuse la pierre",
+        "Significato": "La costanza e la perseveranza conducono all’obiettivo"
+    },
+    {
+        "Idioma": "Être une bonne poire",
+        "Significato": "Essere ingenui"
+    },
+    {
+        "Idioma": "Poser un lapin",
+        "Significato": "Dare buca a qualcuno"
+    },
+    {
+        "Idioma": "Chercher la petite bête",
+        "Significato": "Cercare l’ago nel pagliaio"
+    },
+    {
+        "Idioma": "Avoir un poil dans la main",
+        "Significato": "Essere molto pigri"
+    },
+    {
+        "Idioma": "Casser du sucre sur le dos de quelqu’un",
+        "Significato": "Parlare alle spalle di qualcuno"
+    },
+    {
+        "Idioma": "Péter un cable",
+        "Significato": "Perdere il controllo"
+    },
+    {
+        "Idioma": "Être à l’ouest",
+        "Significato": "Avere la testa tra le nuvole"
+    },
+    {
+        "Idioma": "Être con comme un balai",
+        "Significato": "Essere sciocchi"
+    },
+    {
+        "Idioma": "Ça me gonfle",
+        "Significato": "Ciò mi dà fastidio"
+    },
+    {
+        "Idioma": "Aller se faire cuire un œuf",
+        "Significato": "Lasciar stare qualcuno"
+    },
+    {
+        "Idioma": "Occupe-toi de tes oignons",
+        "Significato": "Fatti i fatti tuoi"
+    },
+    {
+        "Idioma": "Être fauché comme les blés",
+        "Significato": "Essere al verde"
+    },
+    {
+        "Idioma": "Avoir une veine de cocu",
+        "Significato": "Essere molto fortunati"
+    },
+    {
+        "Idioma": "Cracher dans la soupe",
+        "Significato": "Essere ingrati"
+    },
+    {
+        "Idioma": "Cloue le bec",
+        "Significato": "Chiudi il becco"
+    },
+    {
+        "Idioma": "Ca n’a rien à voir avec la choucroute",
+        "Significato": "Non c’entra niente con ciò di cui stiamo parlando"
+    },
+    {
+        "Idioma": "En avoir plein le dos",
+        "Significato": "essere stufi"
+    },
+    {
+        "Idioma": "En avoir plein les bottes",
+        "Significato": "essere stufi"
+    },
+    {
+        "Idioma": "L’encre d’un écolier est plus sacrée que le sang d’un martyr",
+        "Significato": "l’inchiostro di uno scolaro è più sacro del sangue di un martire"
+    },
+    {
+        "Idioma": "Avoir la tête dans les nuages",
+        "Significato": "avere la testa fra le nuvole"
+    },
+    {
+        "Idioma": "Avoir la main heureuse",
+        "Significato": "riuscire in tutto quello che si intraprende"
+    },
+    {
+        "Idioma": "Laisser pisser le mérinos",
+        "Significato": "non cedere alle provocazioni"
+    },
+    {
+        "Idioma": "Quand le chat est parti, les souris dansent",
+        "Significato": "Quando il gatto non c’è, i topi ballano"
+    },
+    {
+        "Idioma": "Etre à côté de ses pompes",
+        "Significato": "Vivere nel proprio mondo"
+    },
+    {
+        "Idioma": "Porter de l’eau à la rivière",
+        "Significato": "Portare qualcosa già presente in abbondanza"
+    },
+    {
+        "Idioma": "Envoyer promener",
+        "Significato": "Mandare a quel paese"
+    },
+    {
+        "Idioma": "Avoir le coup d’œil",
+        "Significato": "Capire al volo"
+    },
+    {
+        "Idioma": "S’ennuyer comme un rat mort",
+        "Significato": "Annoiarsi a morte"
+    },
+    {
+        "Idioma": "Etre faux jeton",
+        "Significato": "Essere ipocriti"
+    },
+    {
+        "Idioma": "Ça roule",
+        "Significato": "Si va avanti"
+    },
+    {
+        "Idioma": "Ne pas savoir sur quel pied danser",
+        "Significato": "non sapere che pesci prendere"
+    },
+    {
+        "Idioma": "Claquer des dents",
+        "Significato": "Battere i denti dal freddo"
+    },
+    {
+        "Idioma": "Faire la sourde oreille",
+        "Significato": "fare il finto tonto"
+    },
+    {
+        "Idioma": "Tenir la jambe à quelqu’un",
+        "Significato": "Infastidire qualcuno"
+    },
+    {
+        "Idioma": "Avoir une dent contre quelqu’un",
+        "Significato": "Avercela con qualcuno"
+    },
+    {
+        "Idioma": "Décrocher la lune",
+        "Significato": "Riuscire in qualcosa di impossibile"
+    },
+    {
+        "Idioma": "Se serrer les coudes",
+        "Significato": "Essere solidale"
+    },
+    {
+        "Idioma": "Avoir la main ouverte",
+        "Significato": "Essere generoso e disponibile"
+    },
+    {
+        "Idioma": "Se faire des cheveux blancs",
+        "Significato": "Preoccuparsi molto di qualcosa"
+    },
+    {
+        "Idioma": "Etre au bout du rouleau",
+        "Significato": "essere stanco fisicamente e mentalmente"
+    },
+    {
+        "Idioma": "Tu m’étonnes !",
+        "Significato": "mi sorprendi"
+    },
+    {
+        "Idioma": "De quoi je me mêle",
+        "Significato": "Di cosa ti impicci?"
+    },
+    {
+        "Idioma": "T’es beau comme un camion",
+        "Significato": "Ti sei messo in tiro"
+    },
+    {
+        "Idioma": "Avaler la pilule",
+        "Significato": "Ingoiare il rospo"
+    },
+    {
+        "Idioma": "Rond comme un boulon",
+        "Significato": "Aver bevuto molto"
+    },
+    {
+        "Idioma": "Faire d’une pierre deux coup",
+        "Significato": "Prendere due piccioni con una fava"
+    },
+    {
+        "Idioma": "Donner sa langue au chat",
+        "Significato": "non rispondere"
+    },
+    {
+        "Idioma": "Fumer comme un pompier",
+        "Significato": "Fumare come un turco"
+    },
+    {
+        "Idioma": "Avoir d’autres chats à fouetter",
+        "Significato": "avere altro a cui pensare"
+    },
+    {
+        "Idioma": "Ca mange pas de pain",
+        "Significato": "Non costa nulla"
+    },
+    {
+        "Idioma": "Chanter come une casserole",
+        "Significato": "Essere stonato come una campana"
+    },
+    {
+        "Idioma": "Ne pas avoir la langue dans sa poche",
+        "Significato": "non tenere mai la bocca chiusa"
+    },
+    {
+        "Idioma": "avoir les crocs",
+        "Significato": "Avere fame"
+    },
+    {
+        "Idioma": "c'est le moment ou jamais",
+        "Significato": "Adesso o mai più"
+    },
+    {
+        "Idioma": "faire des manières",
+        "Significato": "fare i complimenti"
+    },
+    {
+        "Idioma": "à fond de train",
+        "Significato": "A tutta velocità"
+    },
+    {
+        "Idioma": "en avoir plein le dos",
+        "Significato": "Averne piene le scatole"
+    },
+    {
+        "Idioma": "tomber à l'eau",
+        "Significato": "Andare in fumo"
+    },
+    {
+        "Idioma": "de but en blanc",
+        "Significato": "Di punto in bianco"
+    },
+    {
+        "Idioma": "faire la sourde oreille",
+        "Significato": "Fingere di non capire"
+    },
+    {
+        "Idioma": "ficher le camp",
+        "Significato": "Tagliare la corda"
+    },
+    {
+        "Idioma": "il y a anguille sous roche",
+        "Significato": "Qui gatta ci cova"
+    },
+    {
+        "Idioma": "c'est bonnet blanc et blanc bonnet",
+        "Significato": "Se non è zuppa è pan bagnato"
+    },
+    {
+        "Idioma": "en moins de deux",
+        "Significato": "In men che non si dica"
+    },
+    {
+        "Idioma": "il pleut des hallebardes",
+        "Significato": "Piove a catinelle"
+    },
+    {
+        "Idioma": "en avoir par-dessus la tête",
+        "Significato": "averne fin sopra ai capelli"
+    },
+    {
+        "Idioma": "en un tour de main",
+        "Significato": "in un batter d’occhio"
+    },
+    {
+        "Idioma": "à la va-vite",
+        "Significato": "in fretta e furia"
+    },
+    {
+        "Idioma": "se noyer dans une goutte d'eau",
+        "Significato": "affogare in un bicchier d'acqua"
+    },
+    {
+        "Idioma": "un coup d'épée dans l'eau",
+        "Significato": "un buco nell'acqua"
+    },
+    {
+        "Idioma": "faire la causette",
+        "Significato": "fare quattro chiacchiere"
+    },
+    {
+        "Idioma": "avoir du bol",
+        "Significato": "avere fortuna"
+    },
+    {
+        "Idioma": "il y en a marre",
+        "Significato": "ora basta"
+    },
+    {
+        "Idioma": "être à la bourre",
+        "Significato": "essere in ritardo"
+    },
+    {
+        "Idioma": "tomber dans les pommes",
+        "Significato": "Svenire"
+    },
+    {
+        "Idioma": "avoir la chair de poule",
+        "Significato": "avere la pelle d'oca"
+    },
+    {
+        "Idioma": "casser la croûte",
+        "Significato": "fare uno spuntino"
+    },
+    {
+        "Idioma": "quand les poules auront des dents",
+        "Significato": "quando gli asini voleranno"
+    },
+    {
+        "Idioma": "ne pas réveiller le chat qui dort",
+        "Significato": "non svegliare il can che dorme"
+    },
+    {
+        "Idioma": "taper toujours sur le même clou",
+        "Significato": "battere sempre sullo stesso tasto"
+    },
+    {
+        "Idioma": "cela va sans dire",
+        "Significato": "non occorre dirlo"
+    },
+    {
+        "Idioma": "jamais deux sans trois",
+        "Significato": "non c'è due senza tre"
+    },
+    {
+        "Idioma": "cela fait mon affaire",
+        "Significato": "mi conviene"
+    },
+    {
+        "Idioma": "faire le dégoûté",
+        "Significato": "fare lo schizzinoso"
+    },
+    {
+        "Idioma": "à plus forte raison",
+        "Significato": "a maggior ragione"
+    },
+    {
+        "Idioma": "boire un coup",
+        "Significato": "bere un bicchierino"
+    },
+    {
+        "Idioma": "se mettre en quatre",
+        "Significato": "farsi in quattro"
+    },
+    {
+        "Idioma": "vider son sac",
+        "Significato": "vuotare il sacco"
+    },
+    {
+        "Idioma": "avoir le coeur gros",
+        "Significato": "essere triste"
+    },
+    {
+        "Idioma": "mettre aux oubliettes",
+        "Significato": "mettere in dimenticatoio"
+    },
+    {
+        "Idioma": "au pis aller",
+        "Significato": "male che vada"
+    },
+    {
+        "Idioma": "prendre ses cliques et ses claques",
+        "Significato": "fare armi e bagagli"
+    },
+    {
+        "Idioma": "dormir sur ses deux oreilles",
+        "Significato": "dormire fra due guanciali"
+    },
+    {
+        "Idioma": "montrer les talons",
+        "Significato": "alzare i tacchi"
+    },
+    {
+        "Idioma": "casser la baraque",
+        "Significato": "avere successo"
+    },
+    {
+        "Idioma": "à tue-tête",
+        "Significato": "a squarciagola"
+    },
+    {
+        "Idioma": "s'en payer une tranche",
+        "Significato": "spassarsela"
+    },
+    {
+        "Idioma": "arriver comme un cheveu sur la soupe",
+        "Significato": "capitare come i cavoli a merenda"
+    },
+    {
+        "Idioma": "en toutes lettres",
+        "Significato": "chiaro e tondo"
+    },
+    {
+        "Idioma": "finir en queue de poisson",
+        "Significato": "finire in niente"
+    },
+    {
+        "Idioma": "faire la tête",
+        "Significato": "tenere il broncio"
+    },
+    {
+        "Idioma": "à la diable",
+        "Significato": "in fretta a furia"
+    },
+    {
+        "Idioma": "à la queue leu leu",
+        "Significato": "in fila indiana"
+    },
+    {
+        "Idioma": "flambant neuf",
+        "Significato": "nuovo di zecca"
+    },
+    {
+        "Idioma": "fourrer son nez dans quelque chose",
+        "Significato": "ficcare il naso in qualcosa"
+    },
+    {
+        "Idioma": "coup de pompe",
+        "Significato": "colpo di stanchezza"
+    },
+    {
+        "Idioma": "manquer de poigne",
+        "Significato": "non avere polso"
+    },
+    {
+        "Idioma": "boire du petit lait",
+        "Significato": "andare in brodo di giuggiole"
+    },
+    {
+        "Idioma": "ne pas en rater une",
+        "Significato": "non farne una giusta"
+    },
+    {
+        "Idioma": "à tort et à travers",
+        "Significato": "a vanvera"
+    },
+    {
+        "Idioma": "être sans le sou",
+        "Significato": "non avere una lira"
+    },
+    {
+        "Idioma": "à bout de souffle",
+        "Significato": "senza fiato"
+    },
+    {
+        "Idioma": "tout n'est pas dit",
+        "Significato": "non è detta l'ultima parola"
+    },
+    {
+        "Idioma": "à bon entendeur, salut!",
+        "Significato": "a buon intenditor, poche parole"
+    }
+]
